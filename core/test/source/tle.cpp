756b46cdbe3790cf1be9fed79c72cbcf65e0ec97
XLor <yjl9903@vip.qq.com>
2022-10-01 06:23:43 +0800

    chore: more test files

---
+++ core/test/source/tle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int a = 1, b = 1;
+  cin >> a >> b;
+  for (int i = 1; i <= (int)1e9; i++) {
+    a += b; a %= 998244353;
+  }
+  cout << a + b;
+  return 0;
+}
